d63ae8681e13c42e49443b430628ceed41c2e40f
Elise Pham <elise.pham88@gmail.com>
2019-02-21 14:17:41 -0800

    Initial commit

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,9 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  result = 0
+  binary_array.each_index { |i|
+    result += binary_array[i] * 2 ** (binary_array.length - 1 - i)
+  }
+  return result
 end

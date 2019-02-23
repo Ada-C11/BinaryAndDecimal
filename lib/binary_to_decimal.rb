be9b34009cb1e1be379f7dc8bffed310fa2e8b98
qqdipps <sdipp007@gmail.com>
2019-02-22 20:24:38 -0800

    binary_to_decimal methof good, tests pass.

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,3 +1,4 @@
+require "pry"
 # A method named `binary_to_decimal` that receives as input an array of size 8.
 # The array is randomly filled with 0’s and 1’s.
 # The most significant bit is at index 0.
@@ -5,5 +6,15 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  binary_array_length = binary_array.length
+  sum = 0
+  binary_array_length.times do |i|
+    index = (binary_array_length - 1) - i
+    if binary_array[index] == 1
+      sum += 2 ** i
+    end
+  end
+  return sum
 end
+
+p binary_to_decimal([1, 0, 1])

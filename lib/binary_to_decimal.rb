b674070f731d53ade638cdbe4efb4b407a1390cb
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-02-24 13:53:02 -0800

    Passes all tests

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,9 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  sum = 0
+  binary_array.count.times do |i|
+    sum += binary_array[i] * (2 ** (7 - i))
+  end
+  return sum
 end

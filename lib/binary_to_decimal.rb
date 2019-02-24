435762a8555bface12b1ac2a3f4ad81e8322f4b8
Tatiana Quintana <tatsqui@gmail.com>
2019-02-24 15:15:40 -0800

    completed bin-to-dec

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,13 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  base = 0
+  decimal = 0
+  binary_array.reverse.each do |number|
+    if number % 2 != 0
+      decimal += 2**(base)
+    end
+    base += 1
+  end
+  return decimal
 end

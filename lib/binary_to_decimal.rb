ebdcf2c4ad482102017dc1c798ca9bd90b8b0ce9
Tatiana Quintana <tatsqui@gmail.com>
2019-02-24 15:19:17 -0800

    refactored to not use .reverse method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,13 +5,16 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  base = 0
+  base = binary_array.length - 1
   decimal = 0
-  binary_array.reverse.each do |number|
+  binary_array.each do |number|
     if number % 2 != 0
       decimal += 2**(base)
     end
-    base += 1
+    base -= 1
   end
   return decimal
 end
+
+
+

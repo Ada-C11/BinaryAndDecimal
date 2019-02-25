7a462e247fa8541ff0ed74af5342fc9e978ffc0f
Nara <naraa_520@yahoo.com>
2019-02-24 22:38:13 -0800

    Updated the binary to decimal method to pass all the tests.

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,13 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  exponent = 0
+  total = 0
+  i = binary_array.length - 1
+  while i >= 0
+    total += binary_array[i] * 2 ** exponent
+    i -= 1
+    exponent += 1
+  end
+  return total
 end

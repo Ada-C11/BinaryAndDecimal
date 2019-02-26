a3c58ed03ae411c372a31e2617503366a803e41e
Margaret Finnan <msfinnan@gmail.com>
2019-02-25 18:14:40 -0800

    Added binary_to_decimal method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,13 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal_value = 0
+  array_index = binary_array.length - 1
+  exponent_value = 0
+  binary_array.each do
+    decimal_value += binary_array[array_index] * (2 ** exponent_value)
+    exponent_value += 1
+    array_index -= 1
+  end
+  return decimal_value
 end

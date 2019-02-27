1f8891368ac1d4b863eb1eb59bfe7d576eac6e92
Bitaman <b.amani.s@gmail.com>
2019-02-27 08:01:15 -0800

    binary_to_decimal code is done

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,15 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal = 0
+  n = binary_array.length - 1
+  binary_array.each_with_index do |item, index|
+    dec_digit = item * 2 ** n
+    decimal += dec_digit
+    n -= 1
+  end
+  puts "Decimal number for the binary you entered is #{decimal}"
 end
+
+a = [0, 0, 0, 0, 1, 1, 1, 1]
+puts binary_to_decimal(a)

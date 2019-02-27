6ba94ee81371e51619c1242164747eaed58c0a5b
Mina Shin <minamishin@hotmail.com>
2019-02-27 14:53:37 -0800

    binary to decimal

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,9 +1,14 @@
-# A method named `binary_to_decimal` that receives as input an array of size 8.
-# The array is randomly filled with 0’s and 1’s.
-# The most significant bit is at index 0.
-# The least significant bit is at index 7.
-# Calculate  and return the decimal value for this binary number using
-# the algorithm you devised in class.
 def binary_to_decimal(binary_array)
+    power = binary_array.length - 1
+    base_10 = 0
+  
+    binary_array.each do |i|
+      if i == 1
+        base_10 += 2 ** power
+      end
+      power -= 1
+    end
+    puts "#{binary_array} in base 10 is: #{base_10}"
+    return base_10
   raise NotImplementedError
 end

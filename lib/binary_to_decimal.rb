a6bfa4e54dc6b9339588bc71def72f921a482247
Shamira <marshallscm@gmail.com>
2019-02-27 16:49:24 -0800

    finished binary to decimal

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,10 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  decimal = 0
+
+  binary_array.reverse.each_with_index do |num, i|
+    decimal += (num * 2 ** i)
+  end
+  return decimal
 end

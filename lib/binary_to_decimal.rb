2d90e60923d0f1c8190e07bdd9437e369691efb4
K <774162+kaseea@users.noreply.github.com>
2019-03-02 08:49:06 -0800

    for shruti

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,13 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  sum = 0
+  exp = binary_array.length - 1
+  binary_array.each do |x|
+    if x == 1
+      sum += 2 ** exp
+    end
+    exp -= 1
+  end
+  return sum
 end

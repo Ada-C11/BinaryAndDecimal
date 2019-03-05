52d8804738f78b687dcda698e89b509d16753152
Heather Izumi <hkizumi@gmail.com>
2019-03-04 20:34:25 -0800

    Removed extra variable

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,15 +4,15 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+
 def binary_to_decimal(binary_array)
   expected_decimal = 0
   x = 7
   y = 0
-  binary_array.length.times do |digit|
-    digit = binary_array[x] * 2 ** y
+  binary_array.length.times do
+    expected_decimal += binary_array[x] * 2 ** y
     x -= 1
     y += 1
-    expected_decimal += digit
   end
   return expected_decimal
 end
